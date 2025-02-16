33d0b91fd83419e83b88728c2697b556dc7109b7
魔人 <kmlixh@foxmail.com>
2025-02-16 22:25:17 +0800

    简单版本的代码实现

--- crud.go
+++ crud.go
@@ -1,7 +1,14 @@
 package crudo
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
 	"github.com/gin-gonic/gin"
+	"github.com/kmlixh/gom/v4"
+	"github.com/kmlixh/gom/v4/define"
 )
 
 const (
@@ -27,7 +34,7 @@ type CodeMsg struct {
 type ParseRequestFunc func(*gin.Context) (any, error)
 type DataOperationFunc func(any) (any, error)
 type TransferResultFunc func(any) (any, error)
-type BuildResponseFunc func(*gin.Context, any, error) *CodeMsg
+type RenderResponseFunc func(*gin.Context, any, error)
 
 type RequestHandler struct {
 	Method     string
@@ -35,7 +42,7 @@ type RequestHandler struct {
 	ParseRequestFunc
 	DataOperationFunc
 	TransferResultFunc
-	BuildResponseFunc
+	RenderResponseFunc
 }
 
 func (h *RequestHandler) Handle(c *gin.Context) {
@@ -49,9 +56,7 @@ func (h *RequestHandler) Handle(c *gin.Context) {
 	if err == nil && h.TransferResultFunc != nil {
 		result, err = h.TransferResultFunc(result)
 	}
-
-	response := h.BuildResponseFunc(c, result, err)
-	c.JSON(response.Code, response)
+	h.RenderResponseFunc(c, result, err)
 }
 
 func (h *RequestHandler) Register(r *gin.RouterGroup, path string) {
@@ -83,5 +88,322 @@ func (c *Crud) RemoveHandler(path string) {
 func (c *Crud) RegisterRoutes(r *gin.RouterGroup) {
 	for k, v := range c.HandlerMap {
 		v.Register(r, k)
+	}
+}
+
+type QueryParams struct {
+	Table           string           `json:"table"`
+	Page            int              `json:"page"`
+	PageSize        int              `json:"pageSize"`
+	ConditionParams []ConditionParam `json:"conditionParams"`
+	OrderBy         []string         `json:"orderBy"`
+	OrderByDesc     []string         `json:"orderByDesc"`
+}
+type ConditionParam struct {
+	Key    string        `json:"key"`
+	Op     define.OpType `json:"op"`
+	Values []any         `json:"values"`
+}
+
+func NewCrud(table string, db *gom.DB) (*Crud, error) {
+	tableInfo, err := db.GetTableInfo(table)
+	columnMap := TableInfoToColumnMap(tableInfo)
+	if err != nil {
+		return nil, err
+	}
+	return &Crud{HandlerMap: map[string]RequestHandler{
+		PathInsert: {
+			Method:             "POST",
+			PreHandler:         nil,
+			ParseRequestFunc:   RequestToMap,
+			DataOperationFunc:  QueryInsertFunc(db, tableInfo.TableName),
+			TransferResultFunc: DoNothingTransferResultFunc,
+			RenderResponseFunc: RenderJsonResponse,
+		},
+		PathUpdate: {
+			Method:             "POST",
+			PreHandler:         nil,
+			ParseRequestFunc:   RequestToMap,
+			DataOperationFunc:  QueryUpdateFunc(db, tableInfo.TableName),
+			TransferResultFunc: DoNothingTransferResultFunc,
+			RenderResponseFunc: RenderJsonResponse,
+		},
+		PathDelete: {
+			Method:             "GET",
+			PreHandler:         nil,
+			ParseRequestFunc:   RequestToQueryParams(tableInfo.TableName, columnMap),
+			DataOperationFunc:  QueryDeleteFunc(db, tableInfo.TableName),
+			TransferResultFunc: DoNothingTransferResultFunc,
+			RenderResponseFunc: RenderJsonResponse,
+		},
+		PathGet: {
+			Method:             "GET",
+			PreHandler:         nil,
+			ParseRequestFunc:   RequestToQueryParams(tableInfo.TableName, columnMap),
+			DataOperationFunc:  QueryGetFunc(db),
+			TransferResultFunc: DoNothingTransferResultFunc,
+			RenderResponseFunc: RenderJsonResponse,
+		},
+		PathList: {
+			Method:             "GET",
+			PreHandler:         nil,
+			ParseRequestFunc:   RequestToQueryParams(tableInfo.TableName, columnMap),
+			DataOperationFunc:  QueryListFunc(db),
+			TransferResultFunc: DoNothingTransferResultFunc,
+			RenderResponseFunc: RenderJsonResponse,
+		},
+	}}, nil
+}
+func RequestToMap(c *gin.Context) (any, error) {
+	mapData := make(map[string]any)
+	if err := c.ShouldBindJSON(&mapData); err != nil {
+		return nil, err
+	}
+	return mapData, nil
+}
+func RequestToQueryParams(tableName string, columnMap map[string]define.ColumnInfo) ParseRequestFunc {
+	//  从request中
+	return func(c *gin.Context) (any, error) {
+		queryParams := QueryParams{}
+		// 从Request的Query生成一个Map
+		for k, v := range c.Request.URL.Query() {
+			if k == "page" {
+				page, err := strconv.Atoi(v[0])
+				if err != nil {
+					return nil, err
+				}
+				if page < 1 {
+					page = 1
+				}
+				queryParams.Page = page
+			} else if k == "pageSize" {
+				pageSize, err := strconv.Atoi(v[0])
+				if err != nil {
+					return nil, err
+				}
+				if pageSize < 1 {
+					pageSize = 10
+				}
+				queryParams.PageSize = pageSize
+			} else if k == "orderBy" {
+				queryParams.OrderBy = v
+			} else if k == "orderByDesc" {
+				queryParams.OrderByDesc = v
+			} else {
+				// 从k中解析出key和op
+				key, op := KeyToKeyOp(k)
+				column, ok := columnMap[key]
+				if !ok {
+					return nil, fmt.Errorf("column %s not found", key)
+				}
+				values, err := QueryValuesToValues(v, column)
+				if err != nil {
+					return nil, err
+				}
+				queryParams.ConditionParams = append(queryParams.ConditionParams, ConditionParam{
+					Key:    key,
+					Op:     op,
+					Values: values,
+				})
+			}
+		}
+
+		return queryParams, nil
+	}
+}
+func KeyToKeyOp(key string) (string, define.OpType) {
+	keys := strings.Split(key, "_")
+	key = keys[0]
+	opStr := keys[1]
+	op := define.OpEq
+	switch opStr {
+	case "eq":
+		op = define.OpEq
+	case "ne":
+		op = define.OpNe
+	case "gt":
+		op = define.OpGt
+	case "ge":
+		op = define.OpGe
+	case "lt":
+		op = define.OpLt
+	case "le":
+		op = define.OpLe
+	case "in":
+		op = define.OpIn
+	case "notIn":
+		op = define.OpNotIn
+	case "isNull":
+		op = define.OpIsNull
+	case "isNotNull":
+		op = define.OpIsNotNull
+	case "between":
+		op = define.OpBetween
+	case "notBetween":
+		op = define.OpNotBetween
+	case "like":
+		op = define.OpLike
+	case "notLike":
+		op = define.OpNotLike
+	}
+	return key, op
+}
+func TableInfoToColumnMap(tableInfo *define.TableInfo) map[string]define.ColumnInfo {
+	columnMap := make(map[string]define.ColumnInfo)
+	for _, column := range tableInfo.Columns {
+		columnMap[column.Name] = column
+	}
+	return columnMap
+}
+func QueryValuesToValues(values []string, column define.ColumnInfo) ([]any, error) {
+	//将values转换为[]any
+	anyValues := make([]any, len(values))
+	for i, v := range values {
+		switch column.DataType {
+		case "sql.NullString":
+			anyValues[i] = v
+		case "sql.NullInt64":
+			val, err := strconv.ParseInt(v, 10, 64)
+			if err != nil {
+				return nil, err
+			}
+			anyValues[i] = val
+		case "sql.NullFloat64":
+			val, err := strconv.ParseFloat(v, 64)
+			if err != nil {
+				return nil, err
+			}
+			anyValues[i] = val
+		case "sql.NullBool":
+			val, err := strconv.ParseBool(v)
+			if err != nil {
+				return nil, err
+			}
+			anyValues[i] = val
+		case "sql.NullTime":
+			val, err := time.Parse(time.RFC3339, v)
+			if err != nil {
+				return nil, err
+			}
+			anyValues[i] = val
+		case "sql.NullInt32":
+			val, err := strconv.ParseInt(v, 10, 32)
+			if err != nil {
+				return nil, err
+			}
+			anyValues[i] = int32(val)
+		case "sql.NullInt16":
+			val, err := strconv.ParseInt(v, 10, 16)
+			if err != nil {
+				return nil, err
+			}
+			anyValues[i] = int16(val)
+		default:
+			anyValues[i] = v
+		}
+	}
+	return anyValues, nil
+}
+func QueryListFunc(db *gom.DB) DataOperationFunc {
+	return func(i any) (any, error) {
+		if queryParams, ok := i.(QueryParams); ok {
+			chain := db.Chain()
+			chain.Table(queryParams.Table)
+			for _, param := range queryParams.ConditionParams {
+				chain.Where(param.Key, param.Op, param.Values)
+			}
+			for _, orderBy := range queryParams.OrderBy {
+				chain.OrderBy(orderBy)
+			}
+			for _, orderByDesc := range queryParams.OrderByDesc {
+				chain.OrderByDesc(orderByDesc)
+			}
+			chain.Page(queryParams.Page, queryParams.PageSize)
+			result := chain.List()
+			return result, nil
+		}
+		return nil, fmt.Errorf("")
+	}
+}
+func QueryGetFunc(db *gom.DB) DataOperationFunc {
+	return func(i any) (any, error) {
+		if queryParams, ok := i.(QueryParams); ok {
+			chain := db.Chain()
+			chain.Table(queryParams.Table)
+			for _, param := range queryParams.ConditionParams {
+				chain.Where(param.Key, param.Op, param.Values)
+			}
+			result := chain.First()
+			return result.Data, nil
+		}
+		return nil, fmt.Errorf("")
+	}
+}
+func QueryInsertFunc(db *gom.DB, table string) DataOperationFunc {
+	return func(i any) (any, error) {
+		if mapData, ok := i.(map[string]any); ok {
+			chain := db.Chain()
+			chain.Table(table)
+			chain.Values(mapData)
+			result := chain.Save()
+			return result, nil
+		}
+		return nil, fmt.Errorf("")
+	}
+}
+func QueryUpdateFunc(db *gom.DB, table string) DataOperationFunc {
+	return func(i any) (any, error) {
+		if queryParams, ok := i.(QueryParams); ok {
+			chain := db.Chain()
+			chain.Table(queryParams.Table)
+			for _, param := range queryParams.ConditionParams {
+				chain.Where(param.Key, param.Op, param.Values)
+			}
+			result := chain.Delete()
+			return result.Data, nil
+		}
+		return nil, fmt.Errorf("")
+	}
+}
+func QueryDeleteFunc(db *gom.DB, table string) DataOperationFunc {
+	return func(i any) (any, error) {
+		if mapData, ok := i.(map[string]any); ok {
+			chain := db.Chain()
+			chain.Table(table)
+			chain.Where(mapData["id"].(string), define.OpEq, mapData["id"])
+			result := chain.Delete()
+			return result, nil
+		}
+		return nil, fmt.Errorf("")
+	}
+}
+
+func QueryParamsToCondition(queryParams QueryParams) *define.Condition {
+	var condition *define.Condition
+	for i, param := range queryParams.ConditionParams {
+		if i == 0 {
+			condition = define.Eq(param.Key, param.Values[0])
+		} else {
+			condition = condition.And(define.Eq(param.Key, param.Values[0]))
+		}
+	}
+	return condition
+}
+func DoNothingTransferResultFunc(i any) (any, error) {
+	return i, nil
+}
+func RenderJsonResponse(c *gin.Context, data any, err error) {
+	if err != nil {
+		c.JSON(ErrorCode, CodeMsg{
+			Code:    ErrorCode,
+			Data:    nil,
+			Message: err.Error(),
+		})
+	} else {
+		c.JSON(SuccessCode, CodeMsg{
+			Code:    SuccessCode,
+			Data:    data,
+			Message: SuccessMsg,
+		})
 	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/kmlixh/crudo
 go 1.23
 
 toolchain go1.23.1
+
 require github.com/gin-gonic/gin v1.10.0
 
 require (
@@ -18,6 +19,7 @@ require (
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
+	github.com/kmlixh/gom/v4 v4.4.1 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
